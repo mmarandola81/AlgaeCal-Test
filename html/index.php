a7e1310ec8e756574fefb28d249a8c091df51875
Mike Marandola <mmarandola@rswadvertising.com>
2017-10-11 14:48:21 -0400

    add globe reg font, style updates, header updates
    header image background, logo svg update, AlgaeCal variable colors.
    Update default style sheet for video embed, video wrapper for overlay
    tap to play.

--- html/index.php
+++ html/index.php
@@ -1,11 +1,34 @@
 <?php include 'includes/parts/header.php'; ?>
 
-<div class="container">
+<div class="container-fluid">
 	<div class="row">
-		<div class="col-sm-12 col-md-12 col-lg-12">
-			<h1>Suh brah</h1>
-		</div>
+		<section id="main-header-section">
+			<div class="col-sm-12 col-md-12 col-lg-12 image-header">
+				<div class="header-copy">
+					<h1>Introducing</h1>
+					<h2>The ONLY Calcium Supplement that <span>Increases Bone Density*</span> <br>...or Your Money Back</h2>
+				</div>
+			</div>
+		</section>
 	</div>
 </div>
+<div class="container">
+	<section id="video-section"> 
+		<h2>Build brand new bone WITHIN 6 MONTHS with this rare algae calcium <nobr>...even if you're 85!</nobr></h2>
+		<script src="//fast.wistia.com/embed/medias/cecdwaq3dz.jsonp" async></script>
+        <script src="//fast.wistia.com/assets/external/E-v1.js" async></script>
+        <div class="video-wrapper">
+        	<div class="overlay"><i class="fa fa-4x fa-youtube-play" aria-hidden="true"></i></div>
+	        <div class="wistia_responsive_padding" style="padding:56.25% 0 0 0;position:relative;">
+	        	<div class="wistia_responsive_wrapper" style="height:100%;left:0;position:absolute;top:0;width:100%;">
+	        		<div class="wistia_embed wistia_async_cecdwaq3dz videoFoam=true autoPlay=false " style="height:100%;width:100%">&nbsp;
+	        		</div>
+	        	</div>
+	        </div>
+	    </div>
+
+	</section>
+</div>
+
 
 <?php include 'includes/parts/footer.php'; ?>
