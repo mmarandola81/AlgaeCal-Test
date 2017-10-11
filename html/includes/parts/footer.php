9888172485046bbbd4095ae0e334c807a405bc53
Mike Marandola <mmarandola@rswadvertising.com>
2017-10-11 11:30:00 -0400

    initialize

---
+++ html/includes/parts/footer.php
@@ -0,0 +1,4 @@
+
+    <script src="http://<?=$_SERVER['SERVER_NAME']?>/inludes/js/script.js"></script>
+  </body>
+</html>
---
+++ html/index.php
@@ -0,0 +1,11 @@
+<?php include 'includes/parts/header.php'; ?>
+
+<div class="container">
+	<div class="row">
+		<div class="col-sm-12 col-md-12 col-lg-12">
+			<h1>Suh brah</h1>
+		</div>
+	</div>
+</div>
+
+<?php include 'includes/parts/footer.php'; ?>
