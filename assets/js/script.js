27296a7fd003e8ed0a7b2c5ea409f114e54d83a0
Mike Marandola <mmarandola@rswadvertising.com>
2017-10-12 15:19:53 -0400

    styles, css, video play overlay, jquery functions, imagery
    Create product section, buttons, imagery for project. Video overlay,
    export background imagery for highlight and arrow, make as much text
    and css as possible.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,30 @@
 // Algae Scripts
 
+$( document ).ready(function() {
+
+	window._wq = window._wq || [];
+	
+	_wq.push({ id: "cecdwaq3dz", onReady: function(video) {
+	  var video = Wistia.api('cecdwaq3dz');
+
+	  $('.overlay').click(function() {
+			video.play();
+			$(this).fadeOut();
+		});
+
+	  // at 10 seconds, do something amazing
+	  video.bind('secondchange', function(s) {
+	    if (s >= 133)  {
+	      // Insert code to do something amazing here
+	      $('#three-container').fadeIn();
+	      $('html, body').animate({
+		        scrollTop: $('#three-container').offset().top
+		    }, 'slow');
+	      return video.unbind;
+
+	    }
+	  });
+	    
+	}});
+
+});
--- html/index.php
+++ html/index.php
@@ -6,7 +6,7 @@
 			<div class="col-sm-12 col-md-12 col-lg-12 image-header">
 				<div class="header-copy">
 					<h1>Introducing</h1>
-					<h2>The ONLY Calcium Supplement that <span>Increases Bone Density*</span> <br>...or Your Money Back</h2>
+					<h2>The ONLY Calcium Supplement that <span>Increases Bone Density</span><sup>*</sup> <br>...or Your Money Back</h2>
 				</div>
 			</div>
 		</section>
@@ -14,21 +14,64 @@
 </div>
 <div class="container">
 	<section id="video-section"> 
-		<h2>Build brand new bone WITHIN 6 MONTHS with this rare algae calcium <nobr>...even if you're 85!</nobr></h2>
+		<p class="lead">Build <strong>brand new bone</strong> WITHIN 6 MONTHS with this <strong>rare algae</strong> calcium <nobr>...even if you're 85!</nobr></p>
 		<script src="//fast.wistia.com/embed/medias/cecdwaq3dz.jsonp" async></script>
         <script src="//fast.wistia.com/assets/external/E-v1.js" async></script>
         <div class="video-wrapper">
-        	<div class="overlay"><i class="fa fa-4x fa-youtube-play" aria-hidden="true"></i></div>
+        	<div class="overlay">
+        		<div class="overlay-copy">
+        			<i class="fa fa-4x fa-youtube-play" aria-hidden="true"></i><br>
+        			<span class="hand_arrow"></span>Tap to Play</span>
+        		</div>
+        	</div>
 	        <div class="wistia_responsive_padding" style="padding:56.25% 0 0 0;position:relative;">
 	        	<div class="wistia_responsive_wrapper" style="height:100%;left:0;position:absolute;top:0;width:100%;">
-	        		<div class="wistia_embed wistia_async_cecdwaq3dz videoFoam=true autoPlay=false " style="height:100%;width:100%">&nbsp;
+	        		<div id="algaecal-video" class="wistia_embed wistia_async_cecdwaq3dz videoFoam=true autoPlay=false" style="height:100%;width:100%">&nbsp;
 	        		</div>
 	        	</div>
 	        </div>
 	    </div>
-
+	</section>
+	<section id="product-section" class="col-xs-12">
+	    <div id="three-container" class="three-container" style="display: none;"> 
+	    	<div class="col-xs-12 col-sm-4 product-container form-group">
+	    		<p class="most-popular">Most Popular</p>
+	    		<div class="inner-container first">
+	    			<div class="promo-circle"><span class="percent-off">20%</span><br>OFF</div>
+		    		<h2>3 Month Supply</h2>
+		    		<h4>4 AlgaeCal Plus <br>3 Strontium Boost</h4>
+		    		<img class="img-responsive" src="includes/img/bottles.png">
+		    		<h2 class="product-price"><sup class="currency-symbol">$</sup><span class="price-number">233</span> <span class="currency">USD</span></h2>
+		    		<p class="product-price-day lead">Only <strong><span class="currency-symbol">$</span>2.58</strong> / Day</p>
+		    		<a href="https://www.algaecal.com/product/bone-builder-packs/" target="_blank" class="btn btn-block btn-default"> <img class="seal" width="100px" height="100px" src="includes/img/7yrseal.png">Add to Cart&nbsp;&nbsp;<i class="fa fa-play"></i></a>
+		    	</div>
+	    	</div>
+	    	<div class="col-xs-12 col-sm-4 product-container most-popular form-group">
+	    		<p class="most-popular">Most Popular</p>
+	    		<div class="inner-container">
+	    			<div class="promo-circle"><span class="percent-off">20%</span><br>OFF</div>
+		    		<h2>6 Month Supply</h2>
+		    		<h4>4 AlgaeCal Plus <br>3 Strontium Boost</h4>
+		    		<img class="img-responsive" src="includes/img/6mth-bottles.png">
+		    		<h2 class="product-price"><sup class="currency-symbol">$</sup><span class="price-number">233</span> <span class="currency">USD</span></h2>
+		    		<p class="product-price-day lead">Only <strong><span class="currency-symbol">$</span>2.58</strong> / Day</p>
+		    		<a href="https://www.algaecal.com/product/bone-builder-packs/" target="_blank" class="btn btn-block btn-default"> <img class="seal" width="100px" height="100px" src="includes/img/7yrseal.png">Add to Cart&nbsp;&nbsp;<i class="fa fa-play"></i></a>
+		    	</div>
+	    	</div>
+	    	<div class="col-xs-12 col-sm-4 product-container form-group">
+	    		<p class="most-popular">Most Popular</p>
+	    		<div class="inner-container">
+	    			<div class="promo-circle"><span class="percent-off">20%</span><br>OFF</div>
+		    		<h2>12 Month Supply</h2>
+		    		<h4>4 AlgaeCal Plus <br>3 Strontium Boost</h4>
+		    		<img class="img-responsive" src="includes/img/12mth-bottles.png">
+		    		<h2 class="product-price"><sup class="currency-symbol">$</sup><span class="price-number">233</span> <span class="currency">USD</span></h2>
+		    		<p class="product-price-day lead">Only <strong><span class="currency-symbol">$</span>2.58</strong> / Day</p>
+		    		<a href="https://www.algaecal.com/product/bone-builder-packs/" target="_blank" class="btn btn-block btn-default"> <img class="seal" width="100px" height="100px" src="includes/img/7yrseal.png">Add to Cart&nbsp;&nbsp;<i class="fa fa-play"></i></a>
+		    	</div>
+	    	</div>
+	    </div>
 	</section>
 </div>
-
 
 <?php include 'includes/parts/footer.php'; ?>
